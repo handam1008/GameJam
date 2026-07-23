7eaad55a11eac5ea009b8657b83555fccfaaa4a8
pap <projectpaper11@gmail.com>
2026-07-24 02:37:44 +0900

    asdf
    asdf

---
+++ Assets/_Work/PAP/Scripts/Enemy/Bullet.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace _Work.PAP.Scripts.Enemy
+{
+    public class Bullet : MonoBehaviour
+    {
+        [SerializeField] private float speed = 20f;
+        [SerializeField] private LayerMask wallLayer;
+
+        [SerializeField] private int bounceCount = 2;
+        private int currentBounces = 0;
+        Vector2 dir;
+
+        public void Init(Vector2 direction)
+        {
+            dir = direction.normalized;
+            SetRotation();
+        }
+
+        void Update()
+        {
+            float move = speed * Time.deltaTime;
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, move + 0.1f, wallLayer);
+            if (hit.collider != null)
+            {
+                dir = Vector2.Reflect(dir, hit.normal);
+                SetRotation();
+                if (++currentBounces > bounceCount)
+                {
+                    Destroy(gameObject);
+                    //잘하면 풀까지
+                }
+            }
+
+            transform.position += (Vector3)(dir * move);
+        }
+
+        void SetRotation()
+        {
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+}
---
+++ Assets/_Work/PAP/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using _Work.PAP.Scripts.Player;
+using UnityEngine;
+
+namespace _Work.PAP.Scripts.Enemy
+{
+    public class EnemyAI : MonoBehaviour
+    {
+        [SerializeField] private GameObject bullet;
+        [SerializeField] private float cooldown = 1;
+        private float lastFireTime;
+        private PlayerController player;
+        
+
+        private void Start()
+        {
+            player = FindFirstObjectByType<PlayerController>();
+        }
+
+        private void Update()
+        {
+            transform.LookAt(player.transform);
+            SpawnBullet();
+        }
+
+        private void SpawnBullet()
+        {
+            if (Time.time < lastFireTime) return;
+            lastFireTime = Time.time + cooldown;
+            Vector2 dir = (player.transform.position - transform.position).normalized;
+            GameObject bulletInstance = Instantiate(bullet, transform.position, Quaternion.identity);
+            bulletInstance.GetComponent<Bullet>().Init(dir);
+        }
+    }
+}
