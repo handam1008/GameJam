c43b8d93a53e921dbb391dd2168ae2d9dc521057
pap <projectpaper11@gmail.com>
2026-07-24 01:45:34 +0900

    feat
    a

--- Assets/_Work/PAP/Scripts/Agent/AgentMovement.cs
+++ Assets/_Work/PAP/Scripts/Agent/AgentMovement.cs
@@ -1,91 +1,28 @@
 ﻿using System;
-using _Work.PAP.Scripts.Systems;
-using DG.Tweening;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Tilemaps;
-using UnityEngine.UIElements.Experimental;
+using UnityEngine.AI;
 
 namespace _Work.PAP.Scripts.Agent
 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class AgentMovement : MonoBehaviour
     {
-        [field:SerializeField] public TilemapGrid map {get; private set;}
-        [SerializeField] private float moveDuration;
-        [SerializeField] private Ease moveEase;
+        private Vector2 _moveDir;
+        private Rigidbody2D _rb;
 
-        public bool _isMoving { get; private set; }
-        private bool _initialized;
-        private Vector2Int _moveDir;
-        public Vector2Int _facing {get; private set;}
-        public Vector3Int _currentCell { get; private set; }
-
-        public event Action BlockEvent;
+        [SerializeField] private float speed = 5f;
 
         private void Awake()
         {
-            GameManager.Instance.OnMoveEvent += TryStep;
+            _rb = GetComponent<Rigidbody2D>();
         }
 
-        private void Start()
+        private void FixedUpdate()
         {
-            _currentCell = map.WorldToCell(transform.position);
-            if (!map.IsWalkable(_currentCell))
-            {
-                if (map.TryFindNearestWalkable(_currentCell, 8, out Vector3Int fixedCell))
-                {
-                    _currentCell = fixedCell;
-                }
-            }
-            transform.position = map.CellToWorld(_currentCell);
-            GridOccupancy.Reserve(this,_currentCell);
-            _initialized = true;
+            _rb.linearVelocity = _moveDir * speed;
         }
         
-        private void OnEnable()
-        {
-            if (_initialized && map != null) GridOccupancy.Reserve(this, _currentCell);
-        }
-
-        private void OnDisable() => GridOccupancy.ReleaseAll(this);
-        private void OnDestroy()
-        {
-            GridOccupancy.ReleaseAll(this);
-            GameManager.Instance.OnMoveEvent -= TryStep;
-        }
-
-        public void TryStep()
-        {
-            if (_isMoving || _moveDir == Vector2Int.zero || map == null) return;
-            if (_moveDir.x != 0 && _moveDir.y != 0) return;
-
-            _facing = _moveDir;
-            //플립시키기
-
-            Vector3Int target = _currentCell + new Vector3Int(_moveDir.x, _moveDir.y, 0);
-
-            if (!map.IsWalkable(target)) return;
-
-            if (!GridOccupancy.Reserve(this, target))
-            {
-                BlockEvent?.Invoke();
-                return;
-            }
-
-            Vector3Int prevCell = _currentCell;
-            _currentCell = target;
-
-            transform.DOMove(map.CellToWorld(target), moveDuration).OnComplete(() =>
-            {
-                _isMoving = false;
-                _moveDir = Vector2Int.zero;
-            }).SetEase(moveEase);
-            GridOccupancy.Release(prevCell, this);
-            _isMoving = true;
-            return;
-        }
-
-        public void SetMoveDirection(Vector2Int dir)
+        public void SetMovementDir(Vector2 dir)
         {
             _moveDir = dir;
         }
