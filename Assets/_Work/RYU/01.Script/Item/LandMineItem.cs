1a788919964bd2b1f5467cec74ac63006215a8b5
handam1008 <handam10081008@gmail.com>
2026-07-24 22:56:08 +0900

    ㅎ하하

---
+++ Assets/_Work/RYU/01.Script/Item/LandMineItem.cs
@@ -0,0 +1,14 @@
+﻿using _Work.PAP.Scripts.Agent;
+using Cysharp.Threading.Tasks.Triggers;
+using UnityEngine;
+
+namespace _Work.RYU._01.Script.Item
+{
+    [CreateAssetMenu(fileName = "RYU", menuName = "RYU/Item/LandMineItem", order = 0)]
+    public class LandMineItem : AbstractItem
+    {
+        public override void Use(GameObject target)
+        {
+        }
+    }
+}
