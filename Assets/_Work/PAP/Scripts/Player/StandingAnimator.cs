de23edc28793c91f7caf25b6efdc168413fe616f
pap <projectpaper11@gmail.com>
2026-07-24 21:29:31 +0900

    a
    a

---
+++ Assets/_Work/PAP/Scripts/Player/StandingAnimator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Work.PAP.Scripts.Player
+{
+    [RequireComponent(typeof(Animator))]
+    public class StandingAnimator : MonoBehaviour
+    {
+        private Animator animator;
+        private string targetAnim = "IsInf";
+        private int hash;
+
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+            hash = Animator.StringToHash(targetAnim);
+        }
+        
+        public void SetBool(bool value) => animator.SetBool(hash, value);
+    }
+}
