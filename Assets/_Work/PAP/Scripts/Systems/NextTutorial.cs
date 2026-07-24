4d9120babaff764dd0e4933f9b476d987fb08979
pap <projectpaper11@gmail.com>
2026-07-25 08:04:08 +0900

    ads

---
+++ Assets/_Work/PAP/Scripts/Systems/NextTutorial.cs
@@ -0,0 +1,13 @@
+﻿using _Work.PAP.Scripts.TutorialSystem;
+using UnityEngine;
+
+namespace _Work.PAP.Scripts.Systems
+{
+    public class NextTutorial : MonoBehaviour
+    {
+        public void Call()
+        {
+            MainTutorial.Instance.Next();
+        }
+    }
+}
