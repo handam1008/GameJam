5a4824d3e8032269b8cdc94dbdf199a1b7bb77db
pap <projectpaper11@gmail.com>
2026-07-25 09:10:34 +0900

    asd
    asd

---
+++ Assets/_Work/PAP/Scripts/Systems/AnykeyOpen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace _Work.PAP.Scripts.Systems
+{
+    public class AnykeyOpen : MonoBehaviour
+    {
+        [SerializeField] private GameObject target;
+        private void Update()
+        {
+            if (Keyboard.current.anyKey.wasPressedThisFrame)
+            {
+                target.SetActive(true);
+            }
+        }
+    }
+}
